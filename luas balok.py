43d2ee3d05072eda83b98f445c23b9e4cab3940b
SiskaNurawalia <90376230+SiskaNurawalia@users.noreply.github.com>
2021-09-21 16:04:13 +0700

    Add files via upload

---
+++ luas balok.py
@@ -0,0 +1,5 @@
+p = float(input("Masukkan panjang balok: "))
+l = float(input("Masukkan lebar balok: "))
+t = float(input("Masukan tinggi balok: "))
+Volume = p*l*t
+print("Volume balok adalah : "+ str(Volume))
